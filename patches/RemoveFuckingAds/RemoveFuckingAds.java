de554c56cf2ce8a0b2692f2370a743273655df46
Maximoff <maksimoff.andrew.7@gmail.com>
2021-03-01 22:44:48 +0300

    фикс

--- patches/RemoveFuckingAds/RemoveFuckingAds.java
+++ patches/RemoveFuckingAds/RemoveFuckingAds.java
@@ -1,6 +1,6 @@
 // Патч для удаления рекламы
 // Требуется файлы RemoveFuckingAds.smali и rules.txt
-// © Maximoff, 2020
+// © Maximoff, 2021
 
 package ru.maximoff.patch;
 
@@ -15,12 +15,11 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
-import java.util.regex.Pattern;
 
-public class RemoveFuckingAds
-{
+public class RemoveFuckingAds {
 	private String[] checkAds = {
 		"/adbuddiz/",
 		"/adcolony/",
@@ -121,11 +120,13 @@ public class RemoveFuckingAds
 	};
 	private String[] smaliPatterns = {
 		"invoke-.*Lcom/google/android/gms/(internal|ads).*;->addView\\([^\\)]*\\)V",
-		"invoke-.*(/adbuddiz/|/adcolony/|/addapptr/|/adjust/|/adincube/|/adknowledge/|/admarvel/|/admob/|/ads/|/adsdk/|/adserver/adview/|/aerserv/|/airpush/|/altamob/|/appAdForce/|/appbrain/|/appenda/|/applovin/|/appnext/|/appnexus/|/appodeal/|/appia/|/apprupt/|/apsalar/|/avocarrot/|/boxdigital/sdk/ad/|/branch/|/chartboost/|/crashlytics/|/duapps/ad|/fabric/|/flurry/|/fyber/|/google/android/gms/internal/|/greystripe/|/heyzap/|/hyprmx/|/inmobi/|/inneractive/|/instreamatic/|/integralads/|/ironsource/|/jirbo/|/jumptap/|/kochava/|/Leadbolt/|/localytics/|/loopme/|/madsdk/|/mdotm/|/mediabrix/|/millennialmedia/|/mngads/|/moat/|/mobclix/|/mobfox/sdk/|/mobvista/|/mologiq/analytics/|/moolah/|/montexi/|/mopub/|/my/target/|/nexage/|/onelouder/adlib/|/openx/|psm/advertising/|/pubmatic/|/revmob/|/shark/adsert/|/smaato/SOMA/|/smartadserver/|/startapp/|/tagmanager/|/tapjoy/|/unity3d/ads|/vdopia/|/vungle/|/xtify/android/sdk/|/zestadz/android/|ad/AdmobInterstitial|ad/AdmobNative|ads/FullAdmob|NativeAdViewAdmob|InlineAd|/NativeInterstitial|ru/boxdigital/sdk/ad/).*->(hasVideoContent|addHtmlAdView|admob|animateAdView|bannerAdmobMainActivity|beginFetchAds|canBeUsed|displayDownloadImageAlert|doBannerClick|downloadAndDisplayImage|expandAd|fetchAd|forceShowInterstitial|handleShow|incrementMetric|initBanner|initializeAds|initializeAdSDK|internalLoadAd|load|loadAd|loadAdFromBid|loadAds|loadAssetIntoView|loadBanner|loadBannerAd|loadBanners|loadBlocksInfo|loadChildAds|loadCustomEvent|loadData|loadDataWithBaseURL|loadDoAfterService|loadFromServer|loadFullscreen|loadHtml|loadHtmlResponse|loadImages|loadImageView|loadIncentivizedAd|loadInterstitial|loadInterstitialAd|loadList|loadMidPoint|loadNativeAd|loadNativeAds|loadNextAd|loadNextMediatedAd|loadNonJavascript|loadRewardedVideo|loadUrl|loadVideo|loadVideoAds|loadVideoUrl|mraidVideoAddendumInterstitialShow|nativeAdLoaded|onEvent|playVideo|preload|preloadAd|preloadHtml|preloadNativeAdImage|preloadUrl|pushAdsToPool|refreshAds|requestAd|requestBannerAd|requestInterstitial|requestInterstitialAd|setNativeAppwallBanner|setupBanner|shouldShowInterstitial|show|showAd|showAds|showAdInActivity|showAdinternal|showAndRender|showAsInterstitial|showBanner|showBannerAbsolute|showBannerInPlaceholder|showContent|showCustomEventInterstitial|showFullscreen|showIncentivizedAd|showInterstitial|showInterstitialAd|showOfferWall|showMoPubBrowserForUrl|showNativeContentAdView|showNativeAppInstallAdView|showPopup|showPopupExit|showPoststitial|showPreparedVideoFallbackAd|showSplash|showVideo|showWebPage|startAdLoadUponLayout|startMetric|submitAndResetMetrics|isLoading|uploadReports)\\(.*\\)(V|Z)"
+		"invoke-.*(/adbuddiz/|/adcolony/|/addapptr/|/adjust/|/adincube/|/adknowledge/|/admarvel/|/admob/|/ads/|/adsdk/|/adserver/adview/|/aerserv/|/airpush/|/altamob/|/appAdForce/|/appbrain/|/appenda/|/applovin/|/appnext/|/appnexus/|/appodeal/|/appia/|/apprupt/|/apsalar/|/avocarrot/|/boxdigital/sdk/ad/|/branch/|/chartboost/|/crashlytics/|/duapps/ad|/fabric/|/flurry/|/fyber/|/google/android/gms/internal/|/greystripe/|/heyzap/|/hyprmx/|/inmobi/|/inneractive/|/instreamatic/|/integralads/|/ironsource/|/jirbo/|/jumptap/|/kochava/|/Leadbolt/|/localytics/|/loopme/|/madsdk/|/mdotm/|/mediabrix/|/millennialmedia/|/mngads/|/moat/|/mobclix/|/mobfox/sdk/|/mobvista/|/mologiq/analytics/|/moolah/|/montexi/|/mopub/|/my/target/|/nexage/|/onelouder/adlib/|/openx/|psm/advertising/|/pubmatic/|/revmob/|/shark/adsert/|/smaato/SOMA/|/smartadserver/|/startapp/|/tagmanager/|/tapjoy/|/unity3d/ads|/vdopia/|/vungle/|/xtify/android/sdk/|/zestadz/android/|ad/AdmobInterstitial|ad/AdmobNative|ads/FullAdmob|NativeAdViewAdmob|InlineAd|/NativeInterstitial|ru/boxdigital/sdk/ad/).*->(hasVideoContent|addHtmlAdView|admob|animateAdView|bannerAdmobMainActivity|beginFetchAds|canBeUsed|displayDownloadImageAlert|doBannerClick|downloadAndDisplayImage|expandAd|fetchAd|forceShowInterstitial|handleShow|incrementMetric|initBanner|initializeAds|initializeAdSDK|internalLoadAd|load|loadAd|loadAdFromBid|loadAds|loadAssetIntoView|loadBanner|loadBannerAd|loadBanners|loadBlocksInfo|loadChildAds|loadCustomEvent|loadData|loadDataWithBaseURL|loadDoAfterService|loadFromServer|loadFullscreen|loadHtml|loadHtmlResponse|loadImages|loadImageView|loadIncentivizedAd|loadInterstitial|loadInterstitialAd|loadList|loadMidPoint|loadNativeAd|loadNativeAds|loadNextAd|loadNextMediatedAd|loadNonJavascript|loadRewardedVideo|loadUrl|loadVideo|loadVideoAds|loadVideoUrl|mraidVideoAddendumInterstitialShow|nativeAdLoaded|onEvent|playVideo|preload|preloadAd|preloadHtml|preloadNativeAdImage|preloadUrl|pushAdsToPool|refreshAds|requestAd|requestBannerAd|requestInterstitial|requestInterstitialAd|setNativeAppwallBanner|setupBanner|shouldShowInterstitial|show|showAd|showAds|showAdInActivity|showAdinternal|showAndRender|showAsInterstitial|showBanner|showBannerAbsolute|showBannerInPlaceholder|showContent|showCustomEventInterstitial|showFullscreen|showIncentivizedAd|showInterstitial|showInterstitialAd|showOfferWall|showMoPubBrowserForUrl|showNativeContentAdView|showNativeAppInstallAdView|showPopup|showPopupExit|showPoststitial|showPreparedVideoFallbackAd|showSplash|showVideo|showWebPage|startAdLoadUponLayout|startMetric|submitAndResetMetrics|isLoading|uploadReports)\\(.*\\)(V|Z)",
+		"\\.method public(.+?)attachInfo\\(Landroid/content/Context;Landroid/content/pm/ProviderInfo;\\)V([\\s\\S]+?)\"com\\.google\\.android\\.gms\\.ads\\.APPLICATION_ID\"([\\s\\S]+?)\\.end method"
 	};
 	private String[] smaliReplacement = {
 		"invoke-static {}, Lremove/fucking/ads/RemoveFuckingAds;->a()V #Remove-Fucking-Ads; Original: $0",
-		"invoke-static {}, Lremove/fucking/ads/RemoveFuckingAds;->a()$3 #Remove-Fucking-Ads; Original: $0"
+		"invoke-static {}, Lremove/fucking/ads/RemoveFuckingAds;->a()$3 #Remove-Fucking-Ads; Original: $0",
+		".method public$1attachInfo(Landroid/content/Context;Landroid/content/pm/ProviderInfo;)V\n\t.registers 4\n\n\t#Remove-Fucking-Ads\n\treturn-void\n.end method"
 	};
 	private final String[] xmlPatterns = {
 		"<([^>]+)(android:id=\"@id/(?i)((ads?|banner|adview)_?layout)\")([^>]+)android:layout_width=\"[^\"]+ent\"([^>]+)android:layout_height=\"[^\"]+ent\"",
@@ -141,130 +142,96 @@ public class RemoveFuckingAds
 	private FilenameFilter smaliFilter;
 	private FilenameFilter xmlFilter;
 
-    public RemoveFuckingAds()
-	{}
+    public RemoveFuckingAds() {}
 
-    public void patch(String apkPath, String patchPath, String decodePath, String param)
-	{
+    public void patch(String apkPath, String patchPath, String decodePath, String param) {
         File rootDir = new File(decodePath);
-		try
-		{
+		try {
 			getRules(new File(patchPath));
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		dirFilter = new FileFilter()
 		{
 			@Override
-			public boolean accept(File p1)
-			{
+			public boolean accept(File p1) {
 				return p1.isDirectory();
 			}
 		};
 		smaliFilter = new FilenameFilter()
 		{
 			@Override
-			public boolean accept(File p1, String p2)
-			{
+			public boolean accept(File p1, String p2) {
 				return p2.endsWith(".smali");
 			}
 		};
 		xmlFilter = new FilenameFilter()
 		{
 			@Override
-			public boolean accept(File p1, String p2)
-			{
+			public boolean accept(File p1, String p2) {
 				return p2.endsWith(".xml");
 			}
 		};
 		startReplacing(rootDir);
     }
 
-	private void startReplacing(File rootDir)
-	{
+	private void startReplacing(File rootDir) {
 		File[] subDirs = rootDir.listFiles(dirFilter);
-		if (subDirs != null)
-		{
-			for (File subDir : subDirs)
-			{
-				if (subDir.getName().equals("res"))
-				{
+		if (subDirs != null) {
+			for (File subDir : subDirs) {
+				if (subDir.getName().equals("res")) {
 					startReplacing(subDir);
-				}
-				else if (subDir.getName().startsWith("smali"))
-				{
+				} else if (subDir.getName().startsWith("smali")) {
 					replacer(subDir, smaliFilter);
-				}
-				else if (subDir.getName().startsWith("layout"))
-				{
+				} else if (subDir.getName().startsWith("layout")) {
 					replacer(subDir, xmlFilter);
-				}
-				else if (subDir.getName().startsWith("values"))
-				{
+				} else if (subDir.getName().startsWith("values")) {
 					replacer(subDir, xmlFilter);
 				}
 			}
 		}
 	}
 
-	private void replacer(File dir, FilenameFilter filter)
-	{
+	private void replacer(File dir, FilenameFilter filter) {
 		File[] files = dir.listFiles(filter);
 		File[] subDirs = dir.listFiles(dirFilter);
-		if (files != null)
-		{
-			for (File file : files)
-			{
+		if (files != null) {
+			for (File file : files) {
 				String content = readToString(file);
 				if (content == null)
 					continue;
-				if (filter == smaliFilter)
-				{
-					for (int i = 0; i < smaliPatterns.length; i++)
-					{
+				if (filter == smaliFilter) {
+					for (int i = 0; i < smaliPatterns.length; i++) {
 						content = content.replaceAll(smaliPatterns[i], smaliReplacement[i]);
 					}
-				}
-				else
-				{
-					for (int i = 0; i < xmlPatterns.length; i++)
-					{
+				} else {
+					for (int i = 0; i < xmlPatterns.length; i++) {
 						content = content.replaceAll(xmlPatterns[i], xmlReplacement[i]);
 					}
 				}
 				writeToFile(file, content);
 			}
 		}
-		if (subDirs != null)
-		{
-			for (File subDir : subDirs)
-			{
+		if (subDirs != null) {
+			for (File subDir : subDirs) {
 				replacer(subDir, filter);
 			}
 		}
 	}
 
-    private String readToString(File file)
-	{
-		try
-		{
+    private String readToString(File file) {
+		try {
 			String  line;
 			boolean contains = false;
 			StringBuilder response = new StringBuilder();
 			FileInputStream fis = new FileInputStream(file);
 			BufferedReader reader = new BufferedReader(new InputStreamReader(fis, "UTF-8"));
-			while ((line = reader.readLine()) != null)
-			{
+			while ((line = reader.readLine()) != null) {
 				response.append(line).append("\n");
-				if (!contains)
-				{
+				if (!contains) {
 					String lineToLow = line.toLowerCase();
-					for (String check : checkAds)
-					{
-						if (lineToLow.indexOf(check.toLowerCase()) >= 0)
-						{
+					for (String check : checkAds) {
+						if (lineToLow.indexOf(check.toLowerCase()) >= 0) {
 							contains = true;
 							break;
 						}
@@ -272,85 +239,65 @@ public class RemoveFuckingAds
 				}
 			}
 			return (contains ? response.toString() : null);
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 
-	private void writeToFile(File file, String string)
-	{
-		try
-		{
+	private void writeToFile(File file, String string) {
+		try {
 			FileOutputStream fos = new FileOutputStream(file);
 			fos.write(string.getBytes("UTF-8"));
 			fos.close();
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	private void getRules(File patchFile) throws Exception
-	{
+	private void getRules(File patchFile) throws Exception {
         ZipFile zip = new ZipFile(patchFile);
         Enumeration e = zip.entries();
-        while (e.hasMoreElements())
-		{
+        while (e.hasMoreElements()) {
             ZipEntry entry = (ZipEntry) e.nextElement();
-			if (entry.getName().equals("rules.txt"))
-			{
+			if (entry.getName().equals("rules.txt")) {
 				InputStream is = zip.getInputStream(entry);
-				try
-				{
+				try {
 					List<String> rules = parseRules(zip.getInputStream(entry));
-					if (!rules.isEmpty())
-					{
+					if (!rules.isEmpty()) {
 						mergeRules(rules);
 					}
 				}
-				finally
-				{
+				finally {
 					is.close();
 				}
 			}
         }
 	}
 
-	private List<String> parseRules(InputStream input)
-	{
+	private List<String> parseRules(InputStream input) {
 		List<String> result = new ArrayList<String>();
-		try
-		{
+		try {
 			String line;
 			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-			while ((line = reader.readLine()) != null)
-			{
+			while ((line = reader.readLine()) != null) {
 				line = line.trim();
 				if (!line.startsWith("#") && !line.equals(""))
 					result.add(line);
 			}
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return result;
 	}
 
-	private void mergeRules(List<String> rules)
-	{
+	private void mergeRules(List<String> rules) {
 		String[] newCheckAds = new String[checkAds.length + rules.size()];
-		for (int i = 0; i < checkAds.length; i++)
-		{
+		for (int i = 0; i < checkAds.length; i++) {
 			newCheckAds[i] = checkAds[i];
 		}
 		String pattern = "\"[^\"]*(";
-		for (int i = 0; i < rules.size(); i++)
-		{
+		for (int i = 0; i < rules.size(); i++) {
 			if (i > 0)
 				pattern += "|";
 			pattern += Pattern.quote(rules.get(i));
@@ -359,8 +306,7 @@ public class RemoveFuckingAds
 		pattern += ")[^\"]*\"";
 		String[] newSmaliPatterns = new String[smaliPatterns.length + 1];
 		String[] newSmaliReplacement = new String[smaliPatterns.length + 1];
-		for (int i = 0; i < smaliPatterns.length; i++)
-		{
+		for (int i = 0; i < smaliPatterns.length; i++) {
 			newSmaliPatterns[i] = smaliPatterns[i];
 			newSmaliReplacement[i] = smaliReplacement[i];
 		}
